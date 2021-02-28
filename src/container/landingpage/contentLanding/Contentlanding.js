b4c0dd7b13ec6fedf91d3135bc6739370fba3974
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2021-02-28 00:30:31 -0330

    Creating the landing page for the project

---
+++ src/container/landingpage/contentLanding/Contentlanding.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Contentlanding.css';
+
+class ContentLand extends Component{
+    render(){
+        return(
+            <div className="content">
+                <header>Shoeaholic</header>
+                <text>The best shoe store in Town!</text>
+            </div>
+
+        );
+    }
+}
+export default ContentLand;
